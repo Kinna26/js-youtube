9cce6a87f6bda3dd0dc668e191894e38130659bb
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2023-12-26 17:59:49 +0000

    Code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "kinna@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail= "krishna@gmail.com"
+accountPassword="21212"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use var
+because of the issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js as newer version
+
+// alert (3+3) we are using node js, not browser
+
+console.log(3+3)
+console.log("Krishna")
+// code readability should be high, use lines
+
+let name = "Krishna"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigin 
+// string => can use in single court or double but prefer double
+// boolean => true/false
+// null => standalone value
+// undefined => value not given
+// symbol => unique
+
+// object
+
+console.log(typeof "Krishna")
+console.log(typeof age)
+console.log(typeof null)
+
+
