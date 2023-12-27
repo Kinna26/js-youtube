97510a127b9c31a612a504766521ded601c6beb4
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2023-12-27 16:49:46 +0000

    Comparison and data type summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive Data types
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue=100.3
+const isLoggedIn= false
+const outsideTemp = null
+let userEmail; 
+
+
+const id = Symbol('123')
+const anotherId = Symbol ("123")
+console.log(id===anotherId)
+// Above line code will give False because symbol gives 
+// unique identity either we type the same value
+
+// Javascript is Dinamically typed (No need to define type of data type)
+
+// Reference type (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Krishna",
+    age: 22,
+}
+
+const myFunction= function(){
+    console.log("hello world")
+}
+
+console.log(typeof null)
+// above line will return object (thing to remember)
+
+
+
