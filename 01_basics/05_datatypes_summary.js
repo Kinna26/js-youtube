680e2990aa183e87dfeac1ea796e8e2a44fcf421
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2023-12-27 17:05:02 +0000

    stack and heap

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -34,4 +34,28 @@ console.log(typeof null)
 // above line will return object (thing to remember)
 
 
+//************************************ */
 
+// Stack and Heap
+// Stack - Primitive
+// Heap - Non-Primitive
+
+let myyoutubename = "krishna.com"
+let anothername = myyoutubename
+
+anothername = "kinna.com"
+
+console.log(myyoutubename)
+console.log(anothername)
+
+let user1 ={
+    email:"user@google.com",
+    upi:"abc@ybl"
+}
+
+let user2 = user1
+
+user2.email = "kinna@google.com"
+
+console.log(user1)
+console.log(user2)
