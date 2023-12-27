72ae782005dd72659d26b86f1e9f8c8ab86d85cf
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2023-12-27 12:18:45 +0000

    Convertion of datatype

---
+++ 01_basics/03_converandoperation.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/* "33" => 33
+  "33abc" => NaN-> Not a Number
+  true=>1; False=>0;
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false;
+// "" => false
+// "Krishna" => true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
