dac7a037ad264a7eed8df7e60b89457f9daf184f
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2024-01-08 17:38:27 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,7 +1,20 @@
 const tinderUser = new Object() // Singleton Object
 const tinderUser2 = {} // not a singleton object (literal)
 
-console.log(tinderUser)
-console.log(tinderUser2)
+//console.log(tinderUser)
+//console.log(tinderUser2)
 
 // both gives the same value empty object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedin = false
+
+console.log(tinderUser)
+
+const regularUSer = {
+    email : "kinna@gmail.com",
+    fullname : {
+        firstname
+    }
+}
