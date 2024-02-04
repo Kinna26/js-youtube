044161810311b54f570e02ddcb0a9efef59f4f95
Kinna26 <154891888+Kinna26@users.noreply.github.com>
2024-02-04 17:35:39 +0000

    loops

---
+++ 05_Loops.js/01_one.js
@@ -0,0 +1,27 @@
+// for
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+   // console.log(element)
+}
+
+
+for (let i = 0; i <=10; i++) {
+    //console.log(`Table of ${i}`)
+    for (let j = 0; j <= 10 ; j++) {
+       // console.log(`${i} * ${j} = ${i*j}`)
+        
+    }
+    
+}
+
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    
+    //console.log(element)
+}
+
+// break and continue
+
